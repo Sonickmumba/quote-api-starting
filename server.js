35df7877030bae163e399a14c28aee1b2ef8b45a
Sonick Mumba <kingellie.mumba@gmail.com>
2024-06-03 12:18:57 +0200

    comment out code

--- server.js
+++ server.js
@@ -8,30 +8,30 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-app.get('/api/quotes/random', (req, res, next) => {
-  const randomQuote = {quote: getRandomElement(quotes)};
-  res.send(randomQuote);
-})
+// app.get('/api/quotes/random', (req, res, next) => {
+//   const randomQuote = {quote: getRandomElement(quotes)};
+//   res.send(randomQuote);
+// })
 
-app.get('/api/quotes', (req, res, next) => {
-  const person = req.query.person;
-  const filteredArray = quotes.filter((quote) =>quote.person === person)
-  if (person) {
-    res.send({quotes: filteredArray})
-  } else {
-    res.send({quotes: quotes});
-  }
-})
+// app.get('/api/quotes', (req, res, next) => {
+//   const person = req.query.person;
+//   const filteredArray = quotes.filter((quote) =>quote.person === person)
+//   if (person) {
+//     res.send({quotes: filteredArray})
+//   } else {
+//     res.send({quotes: quotes});
+//   }
+// })
 
-app.post('/api/quotes', (req, res, next) => {
-  const {quote, person } = req.query;
-  if (!quote || !person) {
-    res.status(400).send('Query properties do not exit');
-  } else {
-    quotes.push(req.query)
-    res.send({quote: req.query})
-  }
-})
+// app.post('/api/quotes', (req, res, next) => {
+//   const {quote, person } = req.query;
+//   if (!quote || !person) {
+//     res.status(400).send('Query properties do not exit');
+//   } else {
+//     quotes.push(req.query)
+//     res.send({quote: req.query})
+//   }
+// })
 
 app.listen(PORT, () => {
   console.log(`Listening to Port ${PORT}`)
